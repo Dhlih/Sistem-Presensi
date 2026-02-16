34ac3a201ee98c456be6a6e2fe044438272e0767
ifad <yusuffadhlih123@gmail.com>
2026-02-16 18:19:18 +0700

    Added authentication route and dashbord route, Change / endpoint to redirect to login

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render("Home");
+    return redirect("/login");
+});
+
+// Authentication
+Route::get('/login', [AuthController::class, "show_login"]);
+Route::post('/login', [AuthController::class, "login"]);
+Route::post('/logout', [AuthController::class, "logout"]);
+
+Route::get('/dashboard', function () {
+    return Inertia::render("Dashboard");
 });
