b1cfbc991d5e4aa34bfa8cc7f3b1e62796a66d21
ifad <yusuffadhlih123@gmail.com>
2026-02-16 18:17:31 +0700

    Added extends authenticable

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Pengguna extends Model
+class Pengguna extends Authenticatable
 {
     protected $fillable = ["id_pengguna", "username", "password", "jenis_role"];
 
