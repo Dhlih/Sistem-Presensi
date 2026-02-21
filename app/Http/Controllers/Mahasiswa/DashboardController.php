1e02e0bfe17ee5e7e06bf1b4eaaaa41f8f0b4372
ifad <yusuffadhlih123@gmail.com>
2026-02-21 12:14:23 +0700

    Created mahasiswa folder for organizing controller

---
+++ app/Http/Controllers/Mahasiswa/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Mahasiswa;
+
+use App\Http\Controllers\Controller;
+use App\Models\EnrollmentMahasiswa;
+use App\Models\TahunAjaran;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    public function show_dashboard(Request $request)
+    {
+        $pengguna = Auth::user()->load("mahasiswa");
+        $tahun_ajaran  = TahunAjaran::latest()->first();
+        $enrollments = EnrollmentMahasiswa::with("enrollment_dosen.mata_kuliah")->where("id_tahun_ajaran", $tahun_ajaran->id_tahun_ajaran)->where("id_mahasiswa", $pengguna->mahasiswa->id_mahasiswa)->get();
+
+        $jumlah_sks = $enrollments->map(function ($enrollment) {
+            return $enrollment->enrollment_dosen->mata_kuliah->sks ?? 0;
+        })->sum();
+        $jumlah_mata_kuliah = $enrollments->count();
+
+
+        return Inertia::render("Mahasiswa/Dashboard", ["data" => [
+            "jumlah_mata_kuliah" => $jumlah_mata_kuliah,
+            "jumlah_sks" => $jumlah_sks,
+            "tahun_ajaran" => $tahun_ajaran->tahun,
+            "semester" => $tahun_ajaran->semester
+        ]]);
+    }
+}
---
+++ app/Http/Controllers/Mahasiswa/JadwalController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Mahasiswa;
+
+use App\Http\Controllers\Controller;
+use App\Models\EnrollmentMahasiswa;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class JadwalController extends Controller
+{
+    public function show_jadwal_kuliah()
+    {
+        $pengguna = Auth::user()->load("mahasiswa");
+
+        $enrollments = EnrollmentMahasiswa::with([
+            "enrollment_dosen.mata_kuliah",
+            "enrollment_dosen.dosen",
+            "enrollment_dosen.jadwal"
+        ])
+            ->where("id_mahasiswa", $pengguna->mahasiswa->id_mahasiswa)
+            ->get();
+
+        $list_hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
+        $jadwal_reguler = $enrollments->flatMap(function ($enrollment) use ($list_hari) {
+            return $enrollment->enrollment_dosen->jadwal->map(function ($jadwal) use ($enrollment, $list_hari) {
+                return [
+                    "hari" => $list_hari[$jadwal->hari - 1],
+                    "jam_mulai" => $jadwal->jam_mulai,
+                    "jam_selesai" => $jadwal->jam_selesai,
+                    "mata_kuliah" => $enrollment->enrollment_dosen->mata_kuliah->nama,
+                    "dosen" => $enrollment->enrollment_dosen->dosen->nama,
+                    "ruangan" => $jadwal->ruangan,
+                ];
+            });
+        })
+            ->sortBy([
+                ['hari', 'desc'],
+                ['jam_mulai', 'asc'],
+            ])
+            ->values();
+
+        $hari_ini = now()->dayOfWeekIso;
+
+        $jadwal_hari_ini = $enrollments->flatMap(function ($enrollment) use ($hari_ini, $list_hari) {
+            return $enrollment?->enrollment_dosen?->jadwal
+                ->where('hari', $hari_ini)
+                ->map(function ($jadwal) use ($enrollment, $list_hari) {
+                    return [
+                        "mata_kuliah" => $enrollment->enrollment_dosen->mata_kuliah->nama,
+                        "dosen" => $enrollment->enrollment_dosen->dosen->nama,
+                        "hari" => $list_hari[$jadwal->hari - 1],
+                        "ruangan" => $jadwal->ruangan,
+                        "jam_mulai" => $jadwal->jam_mulai,
+                        "jam_selesai" => $jadwal->jam_selesai,
+                    ];
+                });
+        });
+
+        // dd($jadwal_hari_ini->toArray());
+
+        return Inertia::render("Mahasiswa/JadwalKuliah", [
+            "jadwal_reguler" => $jadwal_reguler,
+            "jadwal_hari_ini" => $jadwal_hari_ini
+        ]);
+    }
+}
