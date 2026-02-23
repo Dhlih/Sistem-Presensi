75b586238dd60a01840628e1373d7fd04d91df91
ifad <yusuffadhlih123@gmail.com>
2026-02-23 08:59:47 +0700

    Changed controller name based on role and added new  route for presensi

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Dosen\DashboardController as DosenDashboardController;
+use App\Http\Controllers\Dosen\JadwalController as DosenJadwalController;
+use App\Http\Controllers\PresensiController;
 use App\Http\Controllers\Mahasiswa\DashboardController as MahasiswaDashboardController;
-use App\Http\Controllers\Mahasiswa\JadwalController;
+use App\Http\Controllers\Mahasiswa\JadwalController as MahasiswaJadwalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,17 +18,21 @@ Route::middleware("guest")->group(function () {
 });
 
 Route::middleware("auth")->group(function () {
-    Route::post('/logout', [AuthController::class, "logout"]);
-    
     // Dosen route
     Route::middleware(['role:dosen'])->prefix('dosen')->group(function () {
         Route::get('/dashboard', [DosenDashboardController::class, "show_dashboard"])->name("dashboard");
-        Route::get('/jadwal', [JadwalController::class, "show_jadwal_kuliah"]);
+        Route::get('/jadwal', [DosenJadwalController::class, "show_jadwal_kuliah"]);
+
+        Route::get('/jadwal/{jadwal}/presensi', [PresensiController::class, "show_presensi"]);
+        Route::post('/jadwal/{jadwal}/presensi', [PresensiController::class, "generate_qr_code"]);
     });
 
     // Mahasiswa route
     Route::middleware(['role:mahasiswa'])->prefix('mahasiswa')->group(function () {
         Route::get('/dashboard', [MahasiswaDashboardController::class, "show_dashboard"])->name("dashboard");
-        Route::get('/jadwal', [JadwalController::class, "show_jadwal_kuliah"]);
+        Route::get('/jadwal', [MahasiswaJadwalController::class, "show_jadwal_kuliah"]);
+        Route::get('/mahasiswa/presensi/token/{token}', [PresensiController::class, "scan_qr_code"]);
     });
+
+    Route::post('/logout', [AuthController::class, "logout"]);
 });
