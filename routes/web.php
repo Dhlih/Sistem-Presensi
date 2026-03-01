aaed6125fc4ef60f3d2511f265a2b7985c340fbe
ifad <yusuffadhlih123@gmail.com>
2026-03-01 14:49:14 +0700

    Added riwayat route in dosen and mahasiswa prefix

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Dosen\DashboardController as DosenDashboardController;
 use App\Http\Controllers\Dosen\JadwalController as DosenJadwalController;
+use App\Http\Controllers\Dosen\RiwayatController as DosenRiwayatController;
 use App\Http\Controllers\PresensiController;
 use App\Http\Controllers\Mahasiswa\DashboardController as MahasiswaDashboardController;
 use App\Http\Controllers\Mahasiswa\JadwalController as MahasiswaJadwalController;
+use App\Http\Controllers\Mahasiswa\RiwayatController as MahasiswaRiwayatController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect("/login");
@@ -25,13 +28,16 @@ Route::middleware("auth")->group(function () {
 
         Route::get('/jadwal/{jadwal}/presensi', [PresensiController::class, "show_presensi"]);
         Route::post('/jadwal/{jadwal}/presensi', [PresensiController::class, "generate_qr_code"]);
+
+        Route::get('/riwayat', [DosenRiwayatController::class, "show_riwayat_presensi"]);
     });
 
     // Mahasiswa route
     Route::middleware(['role:mahasiswa'])->prefix('mahasiswa')->group(function () {
         Route::get('/dashboard', [MahasiswaDashboardController::class, "show_dashboard"])->name("dashboard");
         Route::get('/jadwal', [MahasiswaJadwalController::class, "show_jadwal_kuliah"]);
-        Route::get('/mahasiswa/presensi/token/{token}', [PresensiController::class, "scan_qr_code"]);
+        Route::get('/presensi/token/{token}', [PresensiController::class, "scan_qr_code"]);
+        Route::get('/riwayat', [MahasiswaRiwayatController::class, "show_riwayat_presensi"]);
     });
 
     Route::post('/logout', [AuthController::class, "logout"]);
